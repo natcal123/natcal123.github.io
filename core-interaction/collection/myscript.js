79f8d8b4d92b7236fb6021d5dc8943f39b1afc37
Natalie <calln939@newschool.edu>
2018-02-28 20:18:35 -0500

    updated collection

--- core-interaction/collection/myscript.js
+++ core-interaction/collection/myscript.js
@@ -1,10 +1,143 @@
-console.log("hi");
-var countryButton = document.getElementById("countryButton");
+var germanyButton = document.getElementById("germanyButton");
 
-countryButton.addEventListener("mouseenter", handleBtnHover);
+germanyButton.addEventListener("mouseenter", handleBtnHoverGermany);
 
-function handleBtnHover() {
-    var items = document.querySelectAll("");
+function handleBtnHoverGermany() {
+  var items = document.querySelectorAll('.video');
+  items.forEach(function(item) {
+    item.style.display = "none";
+  });
+  var itemsshow = document.querySelectorAll('.country-germany');
+  itemsshow.forEach(function(item) {
+    item.style.display = 'inline-block';
+    // item.classList.add('bigger-videos');
+  });
+}
 
+var USButton = document.getElementById("USButton");
 
+USButton.addEventListener("mouseenter", handleBtnHoverUS);
+
+function handleBtnHoverUS() {
+  var items = document.querySelectorAll('.video');
+  items.forEach(function(item) {
+    item.style.display = "none";
+  });
+  var itemsshow = document.querySelectorAll('.country-us');
+  itemsshow.forEach(function(item) {
+    item.style.display = 'inline-block';
+    // item.classList.add('bigger-videos');
+  });
+}
+
+var UKButton = document.getElementById("UKButton");
+
+UKButton.addEventListener("mouseenter", handleBtnHoverUK);
+
+function handleBtnHoverUK() {
+  var items = document.querySelectorAll('.video');
+  items.forEach(function(item) {
+    item.style.display = "none";
+  });
+  var itemsshow = document.querySelectorAll('.country-uk');
+  itemsshow.forEach(function(item) {
+    item.style.display = 'inline-block';
+    // item.classList.add('bigger-videos');
+  });
+}
+
+var franceButton = document.getElementById("franceButton");
+
+franceButton.addEventListener("mouseenter", handleBtnHoverfrance);
+
+function handleBtnHoverfrance() {
+  var items = document.querySelectorAll('.video');
+  items.forEach(function(item) {
+    item.style.display = "none";
+  });
+  var itemsshow = document.querySelectorAll('.country-france');
+  itemsshow.forEach(function(item) {
+    item.style.display = 'inline-block';
+    // item.classList.add('bigger-videos');
+  });
+}
+
+var netherlandsButton = document.getElementById("netherlandsButton");
+
+netherlandsButton.addEventListener("mouseenter", handleBtnHovernetherlands);
+
+function handleBtnHovernetherlands() {
+  var items = document.querySelectorAll('.video');
+  items.forEach(function(item) {
+    item.style.display = "none";
+  });
+  var itemsshow = document.querySelectorAll('.country-netherlands');
+  itemsshow.forEach(function(item) {
+    item.style.display = 'inline-block';
+    // item.classList.add('bigger-videos');
+  });
+}
+
+var austriaButton = document.getElementById("austriaButton");
+
+austriaButton.addEventListener("mouseenter", handleBtnHoveraustria);
+
+function handleBtnHoveraustria() {
+  var items = document.querySelectorAll('.video');
+  items.forEach(function(item) {
+    item.style.display = "none";
+  });
+  var itemsshow = document.querySelectorAll('.country-austria');
+  itemsshow.forEach(function(item) {
+    item.style.display = 'inline-block';
+    // item.classList.add('bigger-videos');
+  });
+}
+
+var irelandButton = document.getElementById("irelandButton");
+
+irelandButton.addEventListener("mouseenter", handleBtnHoverireland);
+
+function handleBtnHoverireland() {
+  var items = document.querySelectorAll('.video');
+  items.forEach(function(item) {
+    item.style.display = "none";
+  });
+  var itemsshow = document.querySelectorAll('.country-ireland');
+  itemsshow.forEach(function(item) {
+    item.style.display = 'inline-block';
+    // item.classList.add('bigger-videos');
+  });
+}
+
+var africaButton = document.getElementById("africaButton");
+
+africaButton.addEventListener("mouseenter", handleBtnHoverafrica);
+
+function handleBtnHoverafrica() {
+  var items = document.querySelectorAll('.video');
+  items.forEach(function(item) {
+    item.style.display = "none";
+  });
+  var itemsshow = document.querySelectorAll('.country-africa');
+  itemsshow.forEach(function(item) {
+    item.style.display = 'inline-block';
+    // item.classList.add('bigger-videos');
+  });
+}
+
+var canadaButton = document.getElementById("canadaButton");
+
+canadaButton.addEventListener("mouseenter", handleBtnHovercanada);
+
+function handleBtnHovercanada() {
+  var items = document.querySelectorAll('.video');
+  items.forEach(function(item) {
+    item.style.display = "none";
+  });
+  var itemsshow = document.querySelectorAll('.country-canada');
+  itemsshow.forEach(function(item) {
+    item.style.display = 'inline-block';
+    // item.classList.add('bigger-videos');
+  });
 }

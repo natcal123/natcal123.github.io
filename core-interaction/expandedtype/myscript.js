6b6fe84be49ac26b0c45a4d9899756dc36b39530
Natalie <calln939@newschool.edu>
2018-04-04 20:30:46 -0400

    uodate

--- core-interaction/expandedtype/myscript.js
+++ core-interaction/expandedtype/myscript.js
@@ -27,10 +27,56 @@ function changeWord() {
   pos.innerText = my_pairs[clicks][0];
   neg.innerText = my_pairs[clicks][1];
 
+
+  var class_names = [
+    'pair-1',
+    'pair-2',
+    'pair-3',
+    'pair-4',
+    'pair-5',
+    'pair-6',
+    'pair-7',
+    'pair-8',
+    'pair-9',
+    'pair-10',
+    'pair-11',
+  ];
+
+  pos.className = "";
+  neg.className = "";
+  pos.classList.add(class_names[clicks]);
+  neg.classList.add(class_names[clicks]);
+
   clicks += 1;
 
   if (clicks == my_pairs.length) {
     clicks = 0;
   }
+
+  // var my_colors = [
+  //   ['#ff6600', '#00a3c1'],
+  //   ['#ff9400', '#0061b5'],
+  //   ['#ffc600', '#0012a5'],
+  //   ['#ffff00', '#6300a5'],
+  //   ['#8cc700','#c5007c'],
+  //   ['#0fad00', '#ff0000'],
+  //   ['#ff6600', '#00a3c1'],
+  //   ['#ff9400', '#0061b5'],
+  //   ['#ffc600', '#0012a5'],
+  //   ['#ffff00', '#6300a5'],
+  //   ['#8cc700','#c5007c'],
+  //
+  // ];
+  //
+  // pos.style.color = my_colors[clicks][0];
+  // neg.style.color = my_colors[clicks][1];
+  //
+  // clicks += 1;
+  //
+  // if (clicks == my_colors.length) {
+  //   clicks = 0;
+  // }
+
+
 
 }

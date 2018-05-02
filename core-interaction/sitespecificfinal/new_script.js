68bf7f951dccff1300d59043aece1b61801e9b4b
Natalie <calln939@newschool.edu>
2018-05-02 19:38:51 -0400

    knkn

---
+++ core-interaction/sitespecificfinal/new_script.js
@@ -0,0 +1,51 @@
+var currentPage = 1;
+var totalPages = 3;
+
+var swipeZone = document.querySelector('.swipe');
+
+/*
+swipedetect(swipeZone, function(direction){
+  // direction contains either "none", "left", "right", "top", or "down"
+  console.log('swiped ' + direction);
+
+  if (direction == 'left') {
+    if (currentPage <= 1) {
+      pageNumber = 1;
+    } else {
+      currentPage = currentPage - 1;
+    }
+
+    console.log(currentPage);
+    showPage('page-' + currentPage);
+  }
+
+  if (direction == 'right') {
+    if (currentPage >= totalPages) {
+      currentPage = totalPages;
+    } else {
+      currentPage = currentPage + 1;
+    }
+
+    console.log(currentPage);
+    showPage('page-' + currentPage);
+  }
+});
+*/
+
+var navButton1 = document.querySelector('.gobutton');
+
+navButton1.addEventListener('click', function() {
+  showPage('page-2');
+})
+
+var options = document.getElementsByClassName('option');
+
+for (var i = 0; i < options.length; i++) {
+  options[i].addEventListener('click', navigate);
+}
+
+function navigate() {
+  var clicked_option = this.id.slice(-1);
+  showPage('page-' + clicked_option)
+
+}
